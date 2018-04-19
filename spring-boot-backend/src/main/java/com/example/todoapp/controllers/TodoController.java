65eb70be8803845a5f7bdd4aa66598513520b069
Aryeh Berkowitz <ahberko@evoforge.org>
2018-04-19 14:45:47 -0400

    added swagger

--- spring-boot-backend/src/main/java/com/example/todoapp/controllers/TodoController.java
+++ spring-boot-backend/src/main/java/com/example/todoapp/controllers/TodoController.java
@@ -3,6 +3,8 @@ package com.example.todoapp.controllers;
 import javax.validation.Valid;
 import com.example.todoapp.models.Todo;
 import com.example.todoapp.repositories.TodoRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
@@ -11,24 +13,28 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@Api(value="todos", description="Operations pertaining to ToDos")
 @CrossOrigin("*")
 public class TodoController {
 
     @Autowired
     TodoRepository todoRepository;
 
+    @ApiOperation(value = "View a list of available todos", response = List.class)
     @GetMapping("/todos")
     public List<Todo> getAllTodos() {
         Sort sortByCreatedAtDesc = new Sort(Sort.Direction.DESC, "createdAt");
         return todoRepository.findAll(sortByCreatedAtDesc);
     }
 
+    @ApiOperation(value = "Create a new todo")
     @PostMapping("/todos")
     public Todo createTodo(@Valid @RequestBody Todo todo) {
         todo.setCompleted(false);
         return todoRepository.save(todo);
     }
 
+    @ApiOperation(value = "Get a todo by ID", response = Todo.class)
     @GetMapping(value="/todos/{id}")
     public ResponseEntity<Todo> getTodoById(@PathVariable("id") String id) {
         return todoRepository.findById(id)
@@ -36,6 +42,7 @@ public class TodoController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @ApiOperation(value = "Update a todo", response = Todo.class)
     @PutMapping(value="/todos/{id}")
     public ResponseEntity<Todo> updateTodo(@PathVariable("id") String id,
                                            @Valid @RequestBody Todo todo) {
@@ -48,6 +55,7 @@ public class TodoController {
                 }).orElse(ResponseEntity.notFound().build());
     }
 
+    @ApiOperation(value = "Delete a todo")
     @DeleteMapping(value="/todos/{id}")
     public ResponseEntity<?> deleteTodo(@PathVariable("id") String id) {
         return todoRepository.findById(id)
